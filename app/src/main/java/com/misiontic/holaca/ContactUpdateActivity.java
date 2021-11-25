066af607bcd3e7b3c30c5df9a842648b9ffd112e
Camilo A <ing_camiloacg@hotmail.com>
2021-11-25 13:46:21 -0500

    Agrega actualizar y eliminar contacto

--- app/src/main/java/com/misiontic/holaca/ContactUpdateActivity.java
+++ app/src/main/java/com/misiontic/holaca/ContactUpdateActivity.java
@@ -1,10 +1,16 @@
 package com.misiontic.holaca;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.misiontic.holaca.db.MySQLiteHelper;
 
@@ -16,12 +22,15 @@ public class ContactUpdateActivity extends AppCompatActivity {
     private EditText etPhone;
     private EditText etBirthday;
 
+    private int personId;
+    private MySQLiteHelper conexion_bd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_update);
 
-        int personId = getIntent().getIntExtra("person", 0);
+        personId = getIntent().getIntExtra("person", 0);
 
         etName = findViewById(R.id.etUpdatePersonName);
         etLastname = findViewById(R.id.etUpdateLastname);
@@ -29,22 +38,75 @@ public class ContactUpdateActivity extends AppCompatActivity {
         etPhone =findViewById(R.id.etUpdateTelephone);
         etBirthday = findViewById(R.id.etUpdateBirthday);
 
-        cargarContacto(personId);
+        cargarContacto();
+    }
+
+    public void cargarContacto() {
+        conexion_bd = new MySQLiteHelper(this);
+        String sentence = "SELECT * FROM personas WHERE _id = ?";
+        String[] params = new String[]{String.valueOf(personId)};
+        Cursor resultado = conexion_bd.getData(sentence, params);
+        resultado.moveToFirst();
+
+        etName.setText(resultado.getString(1));
+        etLastname.setText(resultado.getString(2));
+        etAddress.setText(resultado.getString(3));
+        etPhone.setText(resultado.getString(4));
+        etBirthday.setText(resultado.getString(5));
 
     }
 
-    public void cargarContacto(int personId) {
-        MySQLiteHelper conexion_bd = new MySQLiteHelper(this);
-        String sentence = "SELECT * FROM personas WHERE _id = ?";
+    public void actualizarContacto(View view) {
+        String tabla = "personas";
+        ContentValues cv = new ContentValues();
+        cv.put("nombres", etName.getText().toString());
+        cv.put("apellidos", etLastname.getText().toString());
+        cv.put("direccion", etAddress.getText().toString());
+        cv.put("telefono", etPhone.getText().toString());
+        cv.put("fecha_nacimiento", etBirthday.getText().toString());
+        String whereClause = "_id = ?";
         String[] params = new String[]{String.valueOf(personId)};
-        Cursor resultados = conexion_bd.getData(sentence, params);
+        int rows = conexion_bd.updateData(tabla, cv, whereClause, params);
+        if (rows > 0) {
+            Toast.makeText(this, "Contacto actualizado", Toast.LENGTH_SHORT).show();
+            goToContacts();
+        } else {
+            Toast.makeText(this, "No se pudo actualizar el contacto", Toast.LENGTH_SHORT).show();
+        }
+    }
 
-        resultados.moveToFirst();
-        etName.setText(resultados.getString(1));
-        etLastname.setText(resultados.getString(2));
-        etAddress.setText(resultados.getString(3));
-        etPhone.setText(resultados.getString(4));
-        etBirthday.setText(resultados.getString(5));
+    public void eliminarContacto(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("¿Está seguro?");
+        builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String tabla = "personas";
+                String whereClause = "_id = ?";
+                String[] params = new String[]{String.valueOf(personId)};
+                int rows = conexion_bd.deleteData(tabla, whereClause, params);
+
+                if (rows > 0) {
+                    Toast.makeText(ContactUpdateActivity.this, "Contacto eliminado", Toast.LENGTH_SHORT).show();
+                    goToContacts();
+                } else {
+                    Toast.makeText(ContactUpdateActivity.this, "No se pudo eliminar el contacto", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // Do nothing
+            }
+        });
+        builder.create();
+        builder.show();
+    }
+
+    public void goToContacts() {
+        Intent intentContact = new Intent(this, ScrollActivity.class);
+        startActivity(intentContact);
     }
 
 }
